82d97061ece4f21f9bb1422efb396d1bf9851a18
Luis Quiñones <lpaandres2020@gmail.com>
2024-02-28 22:36:58 -0500

    bump(nix): v1.1.1

--- default.nix
+++ default.nix
@@ -5,17 +5,17 @@
 }:
 buildGo122Module rec {
   pname = "hyprstfu";
-  version = "1.1.0";
+  version = "1.1.1";
   src = fetchFromGitHub {
     owner = "luisnquin";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-H56T8g9evppa4FZR54CBlchLyCxgTNX12SIJo+RDiCY=";
+    hash = "sha256-LSqeLWFPVcWd1ixza+EW0Q9yVUeTu3U3TlZY7jM/M10=";
   };
 
   ldflags = ["-X main.version=v${version}"];
   buildTarget = ".";
 
-  vendorHash = "sha256-ENrDNIVGnUZ/APhYQaQZCIb/UXLG71yFEqzW9ZK+PPo=";
+  vendorHash = "sha256-5Ahu8N1hV05QkT2y28e6EPHKQq1+YcD6E6mp1b3duEo=";
   doCheck = false;
 }
